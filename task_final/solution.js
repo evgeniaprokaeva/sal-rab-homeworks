051c6684a1688adaaeb0dadc2d6ca21843d42176
Evgeniya <ekarpunina87@gmail.com>
2023-03-18 23:14:49 +0300

    task_final_v2_done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client, goods: [], order: {address, sum}};
+    let data = {goods: [], order: {}};;
 
     let countOfGoods = goods.length;
 
@@ -42,8 +42,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push ({title:goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = street + house + entrance + floor + flat;
-    data.client = name + phone;
+    
+    data.client = name + " "+ phone;
+    data.order.sum = sum;
+    data.order.address = "ул. "+ address.street + "," + " дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
 
        
     let jsonData = JSON.stringify({data});
